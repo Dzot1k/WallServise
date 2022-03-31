fbd457e34abd311d99259abe9c1e260c737f5fc2
Dzot1k <dzot988@mail.ru>
2022-03-31 16:49:55 +0300

    Init commit

---
+++ src/main/kotlin/Post.kt
@@ -0,0 +1,73 @@
+data class Post(
+    val id: Int,
+    val ownerId: Int,
+    val fromId: Int,
+    val createdBy: Int,
+    val date: Int,
+    val text: String,
+    val replyOwnerId: Int,
+    val replyPostId: Int,
+    val friendsOnly: Boolean,
+    val comments: String,
+    val copyright: String,
+    val likes: String,
+    val reposts: String,
+    val views: String,
+    val postType: String,
+    val signerId: Int,
+    val canPin: Boolean = true,
+    val canDelete: Boolean = true,
+    val canEdit: Boolean = true,
+    val isPinned: Boolean = true,
+    val markedAsAds: Boolean = true,
+    val isFavorite: Boolean = true,
+    val donut: String,
+    val postponedId: Int
+) {
+
+}
+
+object WallService {
+    private var posts = emptyArray<Post>()
+    private var postsId = emptyArray<Int>()
+
+    fun add(post: Post): Post {
+        postsId += post.id
+        posts += post.copy(id = postsId.size)
+        return posts.last()
+
+    }
+
+    fun update(post: Post): Boolean {
+        for ((index, postSortOut) in posts.withIndex()) {
+            if (postSortOut.id == post.id) {
+                posts[index] = postSortOut.copy(
+                    id = post.id,
+                    fromId = post.fromId,
+                    createdBy = post.createdBy,
+                    text = post.text,
+                    replyOwnerId = post.replyOwnerId,
+                    replyPostId = post.replyPostId,
+                    friendsOnly = post.friendsOnly,
+                    comments = post.comments,
+                    copyright = post.copyright,
+                    likes = post.likes,
+                    reposts = post.reposts,
+                    views = post.views,
+                    postType = post.postType,
+                    signerId = post.signerId,
+                    canPin = post.canPin,
+                    canDelete = post.canDelete,
+                    canEdit = post.canEdit,
+                    isPinned = post.isPinned,
+                    markedAsAds = post.markedAsAds,
+                    isFavorite = post.isFavorite,
+                    donut = post.donut,
+                    postponedId = post.postponedId
+                )
+                return true
+            }
+        }
+        return false
+    }
+}
---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,291 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class WallServiceTest {
+
+
+    @Test
+    fun add() {
+        val resultPost = WallService.add(
+            Post(
+                id = 0,
+                ownerId = 15,
+                fromId = 15,
+                createdBy = 15,
+                date = 31032022,
+                text = "postText",
+                replyOwnerId = 16,
+                replyPostId = 13,
+                friendsOnly = true,
+                comments = "CommentsObject",
+                copyright = "CopyrightObject",
+                likes = "LikesObject",
+                reposts = "RepostsObject",
+                views = "ViewsObject",
+                postType = "Post",
+                signerId = 15,
+                canPin = true,
+                canDelete = true,
+                canEdit = true,
+                isPinned = true,
+                markedAsAds = true,
+                isFavorite = true,
+                donut = "DonutObject",
+                postponedId = 1
+            )
+        )
+        val expectedResult = resultPost.id
+
+        val actualResult = 0
+
+        assertNotEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun updateFalse() {
+
+        WallService.add(
+            Post(
+                id = 1,
+                ownerId = 15,
+                fromId = 15,
+                createdBy = 15,
+                date = 31032022,
+                text = "postText",
+                replyOwnerId = 16,
+                replyPostId = 13,
+                friendsOnly = true,
+                comments = "CommentsObject",
+                copyright = "CopyrightObject",
+                likes = "LikesObject",
+                reposts = "RepostsObject",
+                views = "ViewsObject",
+                postType = "Post",
+                signerId = 15,
+                canPin = true,
+                canDelete = true,
+                canEdit = true,
+                isPinned = true,
+                markedAsAds = true,
+                isFavorite = true,
+                donut = "DonutObject",
+                postponedId = 1
+            )
+        )
+
+        WallService.add(
+            Post(
+                id = 2,
+                ownerId = 15,
+                fromId = 15,
+                createdBy = 15,
+                date = 31032022,
+                text = "postText",
+                replyOwnerId = 16,
+                replyPostId = 13,
+                friendsOnly = true,
+                comments = "CommentsObject",
+                copyright = "CopyrightObject",
+                likes = "LikesObject",
+                reposts = "RepostsObject",
+                views = "ViewsObject",
+                postType = "Post",
+                signerId = 15,
+                canPin = true,
+                canDelete = true,
+                canEdit = true,
+                isPinned = true,
+                markedAsAds = true,
+                isFavorite = true,
+                donut = "DonutObject",
+                postponedId = 1
+            )
+        )
+
+        WallService.add(
+            Post(
+                id = 3,
+                ownerId = 15,
+                fromId = 15,
+                createdBy = 15,
+                date = 31032022,
+                text = "postText",
+                replyOwnerId = 16,
+                replyPostId = 13,
+                friendsOnly = true,
+                comments = "CommentsObject",
+                copyright = "CopyrightObject",
+                likes = "LikesObject",
+                reposts = "RepostsObject",
+                views = "ViewsObject",
+                postType = "Post",
+                signerId = 15,
+                canPin = true,
+                canDelete = true,
+                canEdit = true,
+                isPinned = true,
+                markedAsAds = true,
+                isFavorite = true,
+                donut = "DonutObject",
+                postponedId = 1
+            )
+        )
+
+        val update = Post(
+            id = 6,
+            ownerId = 15,
+            fromId = 15,
+            createdBy = 15,
+            date = 31032022,
+            text = "postChangingText",
+            replyOwnerId = 16,
+            replyPostId = 13,
+            friendsOnly = true,
+            comments = "CommentsObject",
+            copyright = "CopyrightObject",
+            likes = "LikesObject",
+            reposts = "RepostsObject",
+            views = "ViewsObject",
+            postType = "Post",
+            signerId = 15,
+            canPin = false,
+            canDelete = true,
+            canEdit = true,
+            isPinned = true,
+            markedAsAds = true,
+            isFavorite = false,
+            donut = "DonutObject",
+            postponedId = 1
+        )
+
+        val result = WallService.update(update)
+
+        assertFalse(result)
+
+    }
+
+
+    @Test
+    fun updateTrue() {
+
+        WallService.add(
+            Post(
+                id = 3,
+                ownerId = 15,
+                fromId = 15,
+                createdBy = 15,
+                date = 31032022,
+                text = "postText",
+                replyOwnerId = 16,
+                replyPostId = 13,
+                friendsOnly = true,
+                comments = "CommentsObject",
+                copyright = "CopyrightObject",
+                likes = "LikesObject",
+                reposts = "RepostsObject",
+                views = "ViewsObject",
+                postType = "Post",
+                signerId = 15,
+                canPin = true,
+                canDelete = true,
+                canEdit = true,
+                isPinned = true,
+                markedAsAds = true,
+                isFavorite = true,
+                donut = "DonutObject",
+                postponedId = 1
+            )
+        )
+
+        WallService.add(
+            Post(
+                id = 4,
+                ownerId = 15,
+                fromId = 15,
+                createdBy = 15,
+                date = 31032022,
+                text = "postText",
+                replyOwnerId = 16,
+                replyPostId = 13,
+                friendsOnly = true,
+                comments = "CommentsObject",
+                copyright = "CopyrightObject",
+                likes = "LikesObject",
+                reposts = "RepostsObject",
+                views = "ViewsObject",
+                postType = "Post",
+                signerId = 15,
+                canPin = true,
+                canDelete = true,
+                canEdit = true,
+                isPinned = true,
+                markedAsAds = true,
+                isFavorite = true,
+                donut = "DonutObject",
+                postponedId = 1
+            )
+        )
+
+        WallService.add(
+            Post(
+                id = 5,
+                ownerId = 15,
+                fromId = 15,
+                createdBy = 15,
+                date = 31032022,
+                text = "postText",
+                replyOwnerId = 16,
+                replyPostId = 13,
+                friendsOnly = true,
+                comments = "CommentsObject",
+                copyright = "CopyrightObject",
+                likes = "LikesObject",
+                reposts = "RepostsObject",
+                views = "ViewsObject",
+                postType = "Post",
+                signerId = 15,
+                canPin = true,
+                canDelete = true,
+                canEdit = true,
+                isPinned = true,
+                markedAsAds = true,
+                isFavorite = true,
+                donut = "DonutObject",
+                postponedId = 1
+            )
+        )
+
+        val update = Post(
+            id = 2,
+            ownerId = 15,
+            fromId = 15,
+            createdBy = 15,
+            date = 31032022,
+            text = "postChangingText",
+            replyOwnerId = 16,
+            replyPostId = 13,
+            friendsOnly = true,
+            comments = "CommentsObject",
+            copyright = "CopyrightObject",
+            likes = "LikesObject",
+            reposts = "RepostsObject",
+            views = "ViewsObject",
+            postType = "Post",
+            signerId = 15,
+            canPin = false,
+            canDelete = true,
+            canEdit = true,
+            isPinned = true,
+            markedAsAds = true,
+            isFavorite = false,
+            donut = "DonutObject",
+            postponedId = 1
+        )
+
+        val result = WallService.update(update)
+
+        assertTrue(result)
+    }
+
+}
